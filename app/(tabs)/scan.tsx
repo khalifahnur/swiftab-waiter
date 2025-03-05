73bbdda3fc309e59f327389c014bee28999df860
khalifahnur <khalifahnur1095@gmail.com>
2025-03-05 15:11:35 +0300

    added scan tab

--- app/(tabs)/scan.tsx
+++ app/(tabs)/scan.tsx
@@ -1,16 +1,11 @@
-import { SafeAreaView, StyleSheet } from 'react-native'
+import {StyleSheet } from 'react-native'
 import React from 'react'
-import Container from '@/components/Scan/Container'
 import BarCodeScreen from '@/components/Scan/BarCodeScreen'
 
 
 export default function ScanScreen() {
   return (
-    // <SafeAreaView style={styles.container}>
-    //   <Container />
-    // </SafeAreaView>
-    <BarCodeScreen />
-    
+    <BarCodeScreen /> 
   )
 }
 
