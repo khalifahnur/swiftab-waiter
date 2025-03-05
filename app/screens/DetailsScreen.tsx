4a18a5548b0caf0f0db23b2cbb1111847d299ebb
khalifahnur <khalifahnur1095@gmail.com>
2025-03-05 15:12:08 +0300

    added details screen

--- app/screens/DetailsScreen.tsx
+++ app/screens/DetailsScreen.tsx
@@ -1,26 +1,31 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React, { useLayoutEffect } from 'react'
-import Details from '@/components/Home/Details/Details'
-import { useNavigation } from '@react-navigation/native'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { StyleSheet, Text, View } from "react-native";
+import React, { useLayoutEffect } from "react";
+import Details from "@/components/Home/Details/Details";
+import { useNavigation } from "@react-navigation/native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useLocalSearchParams } from "expo-router";
 
 export default function DetailsScreen() {
-  const navigation = useNavigation()
-  useLayoutEffect(()=>{
+  const navigation = useNavigation();
+
+  const params = useLocalSearchParams();
+  const orderData = params.data ? JSON.parse(params.data as string) : null;
+
+  useLayoutEffect(() => {
     navigation.setOptions({
-      headerShown:false
-    })
-  },[])
+      headerShown: false,
+    });
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
-      <Details />
+      <Details data={orderData}/>
     </SafeAreaView>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    //marginTop:10
-  }
-})
+  container: {
+    flex: 1,
+  },
+});
